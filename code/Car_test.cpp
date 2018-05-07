b11730187e3669c57abbdc3ca1c3c8f1e17fea09
Mo Chen <mochen72@gmail.com>
2018-05-07 15:55:14 -0700

    cleaned up Car_test.cpp a little

--- code/Car_test.cpp
+++ code/Car_test.cpp
@@ -15,59 +15,16 @@
 #include "until.cpp"
 #include "eventually.cpp"
 #include "always.cpp"
+#include "def_extraArgs.cpp"
 
 /**
 @brief Tests the Plane class by computing a reachable set and then computing the optimal trajectory from the reachable set.
 */
 int main(int argc, char *argv[])
 {
-	bool dump_file = false;
+	bool dump_file = true;
 	if (argc >= 2) {
 		dump_file = (atoi(argv[1]) == 0) ? false : true;
-	}
-	bool useTempFile = false;
-	if (argc >= 3) {
-		useTempFile = (atoi(argv[2]) == 0) ? false : true;
-	}
-	const bool keepLast = false;
-	const bool calculateTTRduringSolving = false;
-	levelset::DelayedDerivMinMax_Type delayedDerivMinMax = 
-	  levelset::DelayedDerivMinMax_Disable;
-	if (argc >= 4) {
-		switch (atoi(argv[3])) {
-			default:
-			case 0:
-			delayedDerivMinMax = levelset::DelayedDerivMinMax_Disable;
-			break;
-			case 1:
-			delayedDerivMinMax = levelset::DelayedDerivMinMax_Always;
-			break;
-			case 2:
-			delayedDerivMinMax = levelset::DelayedDerivMinMax_Adaptive;
-			break;
-		}
-	}
-
-	bool useCuda = false;
-	if (argc >= 5) {
-		useCuda = (atoi(argv[4]) == 0) ? false : true;
-	}
-	int num_of_threads = 0;
-	if (argc >= 6) {
-		num_of_threads = atoi(argv[5]);
-	}
-	int num_of_gpus = 0;
-	if (argc >= 7) {
-		num_of_gpus = atoi(argv[6]);
-	}
-	size_t line_length_of_chunk = 1;
-	if (argc >= 8) {
-		line_length_of_chunk = atoi(argv[7]);
-	}
-
-	bool enable_user_defined_dynamics_on_gpu = true;
-	if (argc >= 9) {
-		enable_user_defined_dynamics_on_gpu = (atoi(argv[8]) == 0) ? false : true;
 	}
 
 //!< Compute reachable set
@@ -85,7 +42,7 @@ int main(int argc, char *argv[])
   const beacls::IntegerVec pdDim{2};
 
 // Grid Target and obstacle
-	bool accel = false;
+	bool accel = true;
   const beacls::FloatVec initState{(FLOAT_TYPE)0, (FLOAT_TYPE)25, 
 			(FLOAT_TYPE)(270 * M_PI / 180), (FLOAT_TYPE)15};
 
@@ -103,7 +60,7 @@ int main(int argc, char *argv[])
 
 	if (accel) {
   	g = helperOC::createGrid(gmin, gmax, 
-				beacls::IntegerVec{31,31,21,21}, pdDim);	
+				beacls::IntegerVec{15,15,15,15}, pdDim);	
 	} 
 	else {
 		g = helperOC::createGrid(
@@ -157,36 +114,17 @@ int main(int argc, char *argv[])
 
     // Dynamical system parameters
 		helperOC::DynSysSchemeData* schemeData = new helperOC::DynSysSchemeData;
-		helperOC::HJIPDE_extraArgs extraArgs;
-  
-    // Target set and visualization
-		extraArgs.visualize = true;
+    schemeData->set_grid(g);
 
-		schemeData->set_grid(g);
 		if (accel) {
 			schemeData->dynSys = p4D;
-			extraArgs.plotData.plotDims = beacls::IntegerVec{ 1, 1, 0, 0};
-			extraArgs.plotData.projpt = 
-			beacls::FloatVec{p4D->get_x()[2], p4D->get_x()[3]};			
 		} 
 		else {
 			schemeData->dynSys = p3D;
-			extraArgs.plotData.plotDims = beacls::IntegerVec{ 1, 1, 0};
-			extraArgs.plotData.projpt = beacls::FloatVec{p3D->get_x()[2]};	
 		}
 
-		extraArgs.deleteLastPlot = true;
-		extraArgs.fig_filename = "figs/Car_test";
-
-		extraArgs.execParameters.line_length_of_chunk = line_length_of_chunk;
-		extraArgs.execParameters.calcTTR = calculateTTRduringSolving;
-		extraArgs.keepLast = keepLast;
-		extraArgs.execParameters.useCuda = useCuda;
-		extraArgs.execParameters.num_of_gpus = num_of_gpus;
-		extraArgs.execParameters.num_of_threads = num_of_threads;
-		extraArgs.execParameters.delayedDerivMinMax = delayedDerivMinMax;
-		extraArgs.execParameters.enable_user_defined_dynamics_on_gpu = 
-		enable_user_defined_dynamics_on_gpu;
+    helperOC::HJIPDE_extraArgs extraArgs = 
+      def_extraArgs(accel, schemeData->dynSys);
 
 		std::vector<beacls::FloatVec> alpha_U_beta;
 		int resultU = until(alpha_U_beta, alpha, beta, tau1, tau2, schemeData, tau, 
@@ -200,24 +138,35 @@ int main(int argc, char *argv[])
 		int resultG = always(always_alpha, alpha, tau1, tau2, schemeData, tau, 
 			extraArgs);				
 
-	 //  // save mat file
-		// std::string Car_test_filename("Car_test.mat");
-		// beacls::MatFStream* fs = beacls::openMatFStream(Car_test_filename, 
-		// 	beacls::MatOpenMode_Write);
+	  // save mat file
 
-		// if (dump_file) {
-		// 	beacls::IntegerVec Ns = g->get_Ns();
 
-		// 	g->save_grid(std::string("g"), fs);
-		// 	if (!alpha_U_beta.empty()) {
-		// 		save_vector_of_vectors(alpha_U_beta, std::string("data"), Ns, false, fs);
-		// 	}
-		// 	if (!tau.empty()) {
-		// 		save_vector(tau, std::string("tau"), Ns, false, fs);
-		// 	}
-		// }
+		if (dump_file) {
+			std::string Car_test_filename("Car_test.mat");
+			beacls::MatFStream* fs = beacls::openMatFStream(Car_test_filename, 
+				beacls::MatOpenMode_Write);			
+			beacls::IntegerVec Ns = g->get_Ns();
 
-	  //beacls::closeMatFStream(fs);
+			g->save_grid(std::string("g"), fs);
+			if (!alpha_U_beta.empty()) {
+				save_vector_of_vectors(alpha_U_beta, std::string("alpha_U_beta"), Ns, 
+					false, fs);
+			}
+
+			if (!event_beta.empty()) {
+				save_vector_of_vectors(event_beta, std::string("event_beta"), Ns, 
+					false, fs);
+			}
+
+			if (!always_alpha.empty()) {
+				save_vector_of_vectors(always_alpha, std::string("always_alpha"), Ns, 
+					false, fs);
+			}
+
+			beacls::closeMatFStream(fs);
+		}
+
+	  
 
 		if (schemeData) delete schemeData;
 		if (p3D) delete p3D;
