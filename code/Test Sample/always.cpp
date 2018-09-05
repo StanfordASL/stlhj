cd931e6384518981525186f54742dee571d64bd2
Qizhan Tam <qtam@stanford.edu>
2018-09-05 11:58:44 -0700

    Placed test files into Test Sample folder.

--- code/Test Sample/always.cpp
+++ code/Test Sample/always.cpp
@@ -3,23 +3,22 @@ int always(
     beacls::FloatVec alpha,
     FLOAT_TYPE tau1,
     FLOAT_TYPE tau2,
-    helperOC::DynSysSchemeData* schemeData, 
+    helperOC::DynSysSchemeData* schemeData,
     beacls::FloatVec tau,
     helperOC::HJIPDE_extraArgs extraArgs){
 
-// NOT DONE! MINIMALLY MODIFIED FROM until.cpp
 const FLOAT_TYPE small = 1e-3;
 const size_t numel = schemeData->get_grid()->get_numel();
 
   std::vector<beacls::FloatVec> targets(tau.size());
   for (size_t i = 0; i < tau.size(); ++i) {
     targets[i].assign(numel, 100.);
-    if (tau[i] > tau1 - small && tau[i] < tau2 + small) { 
-      // satisfy beta if tau1 < tau < tau2
+    if (tau[i] > tau1 - small && tau[i] < tau2 + small) {
+      // avoid alpha if tau1 < tau < tau2
      std::copy(alpha.begin(), alpha.end(), targets[i].begin());
     }
   }
-  
+
   schemeData->uMode = helperOC::DynSys_UMode_Max;
   schemeData->dMode = helperOC::DynSys_DMode_Min;
 
@@ -32,7 +31,7 @@ const size_t numel = schemeData->get_grid()->get_numel();
 
   beacls::FloatVec tau_out;
 
-  hjipde->solve(datas, tau_out, extraOuts, targets, tau, schemeData, 
+  hjipde->solve(datas, tau_out, extraOuts, targets, tau, schemeData,
       helperOC::HJIPDE::MinWithType_None, extraArgs);
 
   if (hjipde) delete hjipde;
--- code/Test Sample/until.cpp
+++ code/Test Sample/until.cpp
@@ -1,7 +1,12 @@
-int until(std::vector<beacls::FloatVec>& datas, beacls::FloatVec alpha, 
-  beacls::FloatVec beta, FLOAT_TYPE tau1, FLOAT_TYPE tau2, 
-  helperOC::DynSysSchemeData* schemeData, beacls::FloatVec tau, 
-  helperOC::HJIPDE_extraArgs extraArgs){
+int until(
+    std::vector<beacls::FloatVec>& datas,
+    beacls::FloatVec alpha,
+    beacls::FloatVec beta,
+    FLOAT_TYPE tau1, 
+    FLOAT_TYPE tau2,
+    helperOC::DynSysSchemeData* schemeData,
+    beacls::FloatVec tau,
+    helperOC::HJIPDE_extraArgs extraArgs) {
 
   const FLOAT_TYPE small = 1e-3;
   const size_t numel = schemeData->get_grid()->get_numel();
@@ -9,7 +14,7 @@ int until(std::vector<beacls::FloatVec>& datas, beacls::FloatVec alpha,
   std::vector<beacls::FloatVec> targets(tau.size());
   for (size_t i = 0; i < tau.size(); ++i) {
     targets[i].assign(numel, 100.);
-    if (tau[i] > tau1 - small && tau[i] < tau2 + small) { 
+    if (tau[i] > tau1 - small && tau[i] < tau2 + small) {
       // satisfy beta if tau1 < tau < tau2, but also negate
       std::transform(beta.cbegin(), beta.cend(), targets[i].begin(),
           std::negate<FLOAT_TYPE>());
@@ -18,7 +23,7 @@ int until(std::vector<beacls::FloatVec>& datas, beacls::FloatVec alpha,
 
   std::vector<beacls::FloatVec> obstacles(1);
   obstacles[0].assign(numel, 0.);
-  std::copy(alpha.cbegin(), alpha.cend(), obstacles[0].begin()); 
+  std::copy(alpha.cbegin(), alpha.cend(), obstacles[0].begin());
 
 
   schemeData->uMode = helperOC::DynSys_UMode_Min;
@@ -34,14 +39,14 @@ int until(std::vector<beacls::FloatVec>& datas, beacls::FloatVec alpha,
 
   beacls::FloatVec tau_out;
 
-  hjipde->solve(datas, tau_out, extraOuts, targets, tau, schemeData, 
+  hjipde->solve(datas, tau_out, extraOuts, targets, tau, schemeData,
     helperOC::HJIPDE::MinWithType_None, extraArgs);
-  
+
   // Negate result to follow STL convention
   for (size_t i = 0; i < tau.size(); ++i) {
     std::transform(datas[i].cbegin(), datas[i].cend(), datas[i].begin(),
         std::negate<FLOAT_TYPE>());
-  }  
+  }
 
   if (hjipde) delete hjipde;
   return 0;
